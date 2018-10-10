f4efc00f23abd198756d7318d24f2d1b4430c129
vidyaa Premchander <vidyaa.sv@mountblue.io>
2018-10-10 12:03:24 +0530

    company and job profile

--- jobStop/src/app/app.module.ts
+++ jobStop/src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CompanyProfileComponent } from './company-profile/company-profile.component';
+import { JobprofileComponent } from './jobprofile/jobprofile.component';
+import { CandidateprofileComponent } from './candidateprofile/candidateprofile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CompanyProfileComponent,
+    JobprofileComponent,
+    CandidateprofileComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot([
       {path : 'header' , component : HeaderComponent},
-      {path : 'footer' , component : FooterComponent}
+      {path : 'footer' , component : FooterComponent},
+      {path : 'companyprofile' , component : CompanyProfileComponent},
+      {path : 'candidateprofile', component : CandidateprofileComponent},
+      {path : 'jobprofile', component : JobprofileComponent}
     ])
   ],
   providers: [],
