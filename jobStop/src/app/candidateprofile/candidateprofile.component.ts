0744254ad94c4a4a83cc0d7863d4c8615af9bc0a
vidyaa Premchander <vidyaa.sv@mountblue.io>
2018-10-12 14:43:55 +0530

    forms responsive

--- jobStop/src/app/candidateprofile/candidateprofile.component.ts
+++ jobStop/src/app/candidateprofile/candidateprofile.component.ts
@@ -6,14 +6,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidateprofile.component.css']
 })
 export class CandidateprofileComponent implements OnInit {
+  addProject = false;
+  candidate = {
+  "name": "",
+  "address": "",
+  "emailid": "",
+  "contactnumber": "",
+  "twelfthpercentage": "",
+  "twelfthinstitution": "",
+  "twelfthyear": "",
+  "tenthyear":"",
+  "tenthinstitution": "",
+  "tenthpercentage": "",
+  "uginstitution": "",
+  "specialization": "",
+  "department": "",
+  "ugyear": "",
+  "techskills": "",
+  "projects" : [],
+  }
+  
+  BE = ["Civil Engineering", "Geo Informatics", "Agriculture and Irrigation Engineering", "Mechanical Engineering",
+    "Electronics and Communication Engineering", "Material Science and Engineering", "Mining Engineering",
+    "Industrial Engineering", "Aeronautical Engineering", "Automobile Engineering", " Electrical and Electronics Engineering",
+    "Computer Science and Engineering"];
+  B_Tech = ["Information Technology", "Computer Science and Engineering", " Information Technology",
+    "Chemical Engineering", "Textile Technology", "Industrial Bio-Technology", "Industrial Bio-Technology", "Pharmaceutical Technology",
+    "Rubber and Plastic Technology", " Petroleum Engineering and Technology"];
+  BA = ["Media and Communication", "Culinary Arts", "Mass Communication", "English", "Social Work with Specialisation in Rural Development", "Mass Communication", "English Language and Literature"];
+  arrays = [];
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  addCandidate(){
-    
+  addCandidate() {
+    console.log(this.candidate);
+  }
+  specializationfunc(event) {
+    this.candidate.specialization = event.target.value;
+    if (event.target.value == "B-Tech") {
+      this.arrays = this.B_Tech;
+    }
+    else if (event.target.value == "BE") {
+      this.arrays = this.BE;
+    }
+    else if (event.target.value == "BA") {
+      this.arrays = this.BA;
+    }
   }
 
+  departmentfunc(event) {
+    this.candidate.department = event.target.value;
+  }
+
+  year(event) {
+    this.candidate.ugyear = event.target.value;
+  }
+
+  addProjectFunc() {
+    this.addProject = !this.addProject;
+  }
+
+  addProjectFunc1(value) {
+    this.candidate.projects.push({
+      "pame": value.pname,
+      "pdescription": value.pdescription
+    })
+    console.log(this.candidate.projects);
+    this.addProject = !this.addProject;
+  }
 }
--- jobStop/src/app/company-profile/company-profile.component.ts
+++ jobStop/src/app/company-profile/company-profile.component.ts
@@ -7,33 +7,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./company-profile.component.css']
 })
 export class CompanyProfileComponent implements OnInit {
-  companyname : string;
-  hubs : string;
-  history : string;
-  website : string;
-  description : string;
-  topclients : string;
-  emailids : string;
-  contactnumber : string;  
-  address : string;
+  company = {
+  "companyname" : "",
+  "hubs" : "",
+  "history" : "",
+  "website" : "",
+  "description" : "",
+  "topclients" : "",
+  "emailids" : "",
+  "contactnumber" : "",  
+  "address" : ""
+  }
+  
   
   constructor(private router : Router) { }
 
   ngOnInit() {
   }
 
-  addCompany(value){
-    this.companyname = value.companyname;
-    this.hubs = value.hubs;
-    this.history = value.history;
-    this.website = value.website;
-    this.description = value.description;
-    this.topclients = value.topclients;
-    this.emailids = value.emailids;
-    this.contactnumber = value.contactnumber;
-    this.address = value.address;
-    console.log(this.companyname, this.hubs, this.history, this.website, this.description, this.topclients, this.emailids, this.contactnumber, this.address);
-
+  addCompany(){
+    console.log(this.company);
   }
   
 }
