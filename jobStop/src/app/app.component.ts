bf6e75102c31110fdf872a640208e7da27a740ec
inderjot.singh <inderjot.singh@mountblue.io>
2018-10-16 12:23:02 +0530

    experiment

---
+++ jobStop/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+<<<<<<< HEAD
+  styleUrls: ['./app.component.=scss']
+})
+export class AppComponent {
+  title = 'jobStop';
+=======
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+>>>>>>> 1ee1eac32d090657e10fc38d57e5776b63290900
+}
---
+++ jobStop/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+<<<<<<< HEAD
+  production: false
+=======
+  production: false,
+  firebaseConfig: {
+    apiKey: " AIzaSyBhSkNA4Z6Pv_zWxpJfCpQ0_KotoaPA-rk",
+    authDomain: "codeslayers-jobstop.firebaseapp.com",
+    databaseURL: "https://codeslayers-jobstop.firebaseio.com",
+    projectId: "codeslayers-jobstop",
+  }
+>>>>>>> 1ee1eac32d090657e10fc38d57e5776b63290900
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
