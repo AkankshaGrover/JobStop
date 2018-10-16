428a944a99ef561dee9beb67e7e17175461d7a76
Your Nameshubham <you@example.comshubham.rathore@mountblue.io>
2018-10-16 13:59:10 +0530

    applicants able to see their status

--- jobStop/src/app/companies/companies.component.ts
+++ jobStop/src/app/companies/companies.component.ts
@@ -5,6 +5,7 @@ import { UserService } from "../services/user.service";
 import { SessionStorageService } from 'ngx-webstorage';
 
 
+
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.component.html',
@@ -15,8 +16,8 @@ export class CompaniesComponent implements OnInit {
   item: Observable<any[]>;
   constructor(private db: AngularFireDatabase, private session: SessionStorageService, private userService: UserService) {
     this.items = db.list('company/').valueChanges();
-    this.items.subscribe(res =>
-      console.log(res));
+    // this.items.subscribe(res =>
+    //   console.log(res));
 
     // db.database.ref('company/').orderByKey();
     // this.item.subscribe(res =>
@@ -27,7 +28,8 @@ export class CompaniesComponent implements OnInit {
   apply(item, job, i) {
     console.log(item.uid)
     this.session.retrieve('user');
-    this.db.database.ref('/company/' + item.uid + '/candidatesApplied').push({ 'uid': this.session.retrieve('user')[0].uid })
+    this.db.database.ref('/company/' + item.uid + '/candidatesApplied').push({ 'uid':  this.session.retrieve('user')[0].uid})
+    this.db.database.ref('/candidate/' + this.session.retrieve('user')[0].uid + '/companiesApplied').push({ 'uid': item.uid })
   }
 }
 
